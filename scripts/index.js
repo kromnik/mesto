f77fa6c98e848d718a0d54cd91e166cee17988be
Roki K <kiroutp@yandex.ru>
2022-03-24 20:20:48 +0300

    Исправление замечаний_ревью_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,41 @@
 const popupElement = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__edit-button');
 const closePopup = document.querySelector('.popup__close-button');
+let formElement = document.querySelector ('.popup__form');
+let nameInput = formElement.querySelector ('.popup__userinfo_info_username');
+let jobInput = formElement.querySelector ('.popup__userinfo_info_username-info');
+let profileUsername = document.querySelector ('.profile__profile-info-username');
+let profileUsernameInfo = document.querySelector ('.profile__profile-info-username-info');
 
-const ESC_KEY = 'Escape';
+// const ESC_KEY = 'Escape';
 
 function popupOpen() {
   popupElement.classList.add ('popup_opened');
-  document.addEventListener('keyup', onDocumentKeyUp);
+  nameInput.value = profileUsername.textContent;
+  jobInput.value = profileUsernameInfo.textContent;
+  // document.addEventListener('keyup', onDocumentKeyUp);
 }
 
 function popupClose() {
   popupElement.classList.remove ('popup_opened');
-  document.removeEventListener('keyup', onDocumentKeyUp);
+  // document.removeEventListener('keyup', onDocumentKeyUp);
 }
 
-function onDocumentKeyUp(event) {
-  if (event.key === ESC_KEY) {
-      popupClose();
-  }
-}
+// function onDocumentKeyUp(event) {
+//   if (event.key === ESC_KEY) {
+//       popupClose();
+//   }
+// }
 
-openPopup.addEventListener ('click', (event) => {
-  popupOpen()
-}
-);
+openPopup.addEventListener ('click', popupOpen);
 
 closePopup.addEventListener ('click', popupClose);
 
-let formElement = document.querySelector ('.popup__form');
-let nameInput = formElement.querySelector ('.popup__username');
-let jobInput = formElement.querySelector ('.popup__username-info');
-let profileUsername = document.querySelector ('.profile__profile-info-username');
-let profileUsernameInfo = document.querySelector ('.profile__profile-info-username-info');
-
-nameInput.value = profileUsername.textContent;
-jobInput.value = profileUsernameInfo.textContent;
-
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  
   profileUsername.textContent = nameInput.value;
   profileUsernameInfo.textContent = jobInput.value;
-
-  popupClose('submit');
-  
+  popupClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
